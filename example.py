d491f9c6ed350027ca659bb0d18586409a50f517
Артём Костин <superriki228228@gmail.com>
2022-12-07 17:44:32 +0300

    change golden_ration_naive method

--- example.py
+++ example.py
@@ -3,8 +3,8 @@ import numpy as np
 import math
 
 def f(x):
-    return math.sin(x)
+    return abs(x-0.5)
 
-print(OptimizationMethods.half_segment(function=f,a=0,b=10))
+print(OptimizationMethods.half_segment(function=f,a=0,b=1))
 
-print(OptimizationMethods.golden_ration_naive(function=f,a=0,b=10))
+print(OptimizationMethods.golden_ration_naive(function=f,a=0,b=1))
--- mlib.py
+++ mlib.py
@@ -3,23 +3,22 @@ import math
 
 class OptimizationMethods:
 
-    def half_segment(function,a,b,delta=None,epsilon=None):
+    def half_segment(function,a,b,delta=None,epsilon=None,max_iter=None):
         if(delta==None): delta=1e-7
         if(epsilon==None): epsilon=1e-6
-        max_iter=2*(int)(math.log((b-a-delta)/(epsilon-delta)))+1
+        if(max_iter==None): max_iter=2*(int)(math.log((b-a-delta)/(epsilon-delta)))+1
         for i in range(max_iter):
             x_1=(a+b-delta)/2
             x_2=(a+b+delta)/2
             if(function(x_1)<=function(x_2)): b=x_2
             else: a=x_1
-        return function(x_1) if function(x_1)<=function(x_2) else function(x_2)
+        return min(function(x_1),function(x_2))
 
-    def golden_ration_naive(function,a,b,delta=None,epsilon=None):
-        if(delta==None): delta=1e-7
+    def golden_ration_naive(function,a,b,epsilon=None,max_iter=None):
         if(epsilon==None): epsilon=1e-6
-        golden_1 = (3 - 5 ** 0.5) / 2
-        golden_2 = (5 ** 0.5 - 1) / 2
-        max_iter=2*(int)(math.log((b-a-delta)/(epsilon-delta)))+1
+        golden_1 = (3 - math.sqrt(5)) / 2
+        golden_2 = (math.sqrt(5) - 1) / 2
+        if(max_iter==None): max_iter=(int)(math.log(epsilon/(b-a))/math.log(golden_2))+1
         for i in range(max_iter):
             x_1=a+(b-a)*golden_1
             x_2=a+(b-a)*golden_2
@@ -29,7 +28,7 @@ class OptimizationMethods:
             else: 
                 a=x_1
                 x=x_2
-        return function(a+b-x) if function(a+b-x)<=function(x) else function(x)
+        return min(function(a+b-x),function(x))
 
 
 
