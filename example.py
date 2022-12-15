e6b3aa88679effe4ecaf3b0ab35a507137de6ebd
Артём Костин <superriki228228@gmail.com>
2022-12-16 01:53:38 +0300

    implement gradient of function

--- example.py
+++ example.py
@@ -4,13 +4,29 @@ import numpy as np
 import math
 
 def f(x):
-    return x*x
-
-def f(x1,x2):
-    return x1+x2
+    return x[0]*x[1]+3*x[0]-2*x[1]
 
 #print(OptimizationMethods.half_segment(function=f,a=0,b=1))
 
 #print(OptimizationMethods.golden_ration_naive(function=f,a=0,b=1))
 
-print(MathObjects.first_derivate(function=f,point=1))
+#print(MathObjects.first_derivate(function=f,point=[1,2],num_var=1))
+
+m=MathObjects()
+
+#print(f([3,2]))
+
+#print(m.first_derivative_n_var(function=f,point=[3,1],num_var=0))
+
+#print(m.gradient(function=f,point=[1,2]))
+
+def g(x):
+    return x**2+x
+
+#print(m.first_derivative_1_var(g,7))
+
+#print(m.add_num([1,2,3,4],0,5))
+
+print(m.first_derivative_n_var(f,[1,2],0))
+
+print(m.gradient(f,[1,2]))
--- mlib.py
+++ mlib.py
@@ -3,21 +3,37 @@ import math
 
 class MathObjects:
 
-    def first_derivate(function,point,epsilon=None):
-        if(epsilon==None): epsilon=1e-6
+
+    def add_value(self,list,index,value):
+        return list[:index]+[list[index]+value]+list[index+1:]
+
+    def first_derivative_1_var(self,function,point,epsilon=None):
+        if(epsilon==None): epsilon=1e-8
         h=1
-        diff=(function(point+h)-function(point))/h
-        h=h/2
-        while(abs(diff-(function(point+h)-function(point))/h)>=epsilon):
-            diff=(function(point+h)-function(point))/h
+        diff=0
+        while(abs(diff-(function(point+h)-function(point-h))/(2*h))>=epsilon):
+            diff=(function(point+h)-function(point-h))/(2*h)
             h=h/2
         return diff
 
+    def first_derivative_n_var(self,function,point,num_var,epsilon=None):
+        if(epsilon==None): epsilon=1e-8
+        h=1
+        diff=0
+        while(abs(diff-(function(self.add_value(point,num_var,h))-function(self.add_value(point,num_var,-h)))/(2*h))>=epsilon):
+            diff=(function(self.add_value(point,num_var,h))-function(self.add_value(point,num_var,-h)))/(2*h)
+            h=h/2
+        return diff
+
+    def gradient(self,function,point,epsilon=None):
+        if(epsilon==None): epsilon=1e-8
+        return [self.first_derivative_n_var(function,point,i,epsilon) for i in range(len(point))]
+
 class OptimizationMethods:
 
-    def half_segment(function,a,b,delta=None,epsilon=None,max_iter=None):
-        if(delta==None): delta=1e-7
-        if(epsilon==None): epsilon=1e-6
+    def half_segment(self,function,a,b,delta=None,epsilon=None,max_iter=None):
+        if(delta==None): delta=1e-9
+        if(epsilon==None): epsilon=1e-8
         if(max_iter==None): max_iter=2*(int)(math.log((b-a-delta)/(epsilon-delta)))+1
         for i in range(max_iter):
             x_1=(a+b-delta)/2
@@ -26,7 +42,7 @@ class OptimizationMethods:
             else: a=x_1
         return min(function(x_1),function(x_2))
 
-    def golden_ration_naive(function,a,b,epsilon=None,max_iter=None):
+    def golden_ration_naive(self,function,a,b,epsilon=None,max_iter=None):
         if(epsilon==None): epsilon=1e-6
         golden_1 = (3 - math.sqrt(5)) / 2
         golden_2 = (math.sqrt(5) - 1) / 2
@@ -42,6 +58,6 @@ class OptimizationMethods:
                 x=x_2
         return min(function(a+b-x),function(x))
 
-    def gradient_iter(function,a,b,delta=None,epsilon=None,max_iter=None):
+    def gradient_iter(self,function,a,b,delta=None,epsilon=None,max_iter=None):
         
         return 0
