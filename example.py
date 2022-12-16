e8a179e6110f311d0c24ae2add35bd3ea4e49529
Артём Костин <superriki228228@gmail.com>
2022-12-16 17:54:29 +0300

    implement gradient method

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 from mlib import OptimizationMethods
-from mlib import MathObjects
 import numpy as np
 import math
 
+# def f(x):
+#     return x[0]*x[1]+3*x[0]*x[2]-2*x[1]+np.sin(x[2])
+
 def f(x):
-    return x[0]*x[1]+3*x[0]-2*x[1]
+    return x[0]**2+np.sin(x[1])
 
 #print(OptimizationMethods.half_segment(function=f,a=0,b=1))
 
@@ -12,7 +14,7 @@ def f(x):
 
 #print(MathObjects.first_derivate(function=f,point=[1,2],num_var=1))
 
-m=MathObjects()
+o=OptimizationMethods()
 
 #print(f([3,2]))
 
@@ -21,12 +23,20 @@ m=MathObjects()
 #print(m.gradient(function=f,point=[1,2]))
 
 def g(x):
-    return x**2+x
+    return 3
 
 #print(m.first_derivative_1_var(g,7))
 
 #print(m.add_num([1,2,3,4],0,5))
 
-print(m.first_derivative_n_var(f,[1,2],0))
+#print(o.first_derivative_n_var(f,[1,2],0))
 
-print(m.gradient(f,[1,2]))
+#print(o.gradient(f,[1,2]))
+
+#print([(x+y)/2 for x,y in zip([1,2,3],[2,3,5])])
+
+#print(o.half_segment(g,1,2))
+#print(o.golden_ration_naive(g,1,2))
+print(o.gradient_method(f,[-1,-1],[2,2]))
+
+#print([0]*3)
--- mlib.py
+++ mlib.py
@@ -1,8 +1,13 @@
 import numpy as np
 import math
 
-class MathObjects:
+class OptimizationMethods:
 
+    def gradient_zero(self,grad,epsilon):
+        for i in range(len(grad)):
+            if(grad[i]>=epsilon):
+                return False
+        return True
 
     def add_value(self,list,index,value):
         return list[:index]+[list[index]+value]+list[index+1:]
@@ -29,8 +34,6 @@ class MathObjects:
         if(epsilon==None): epsilon=1e-8
         return [self.first_derivative_n_var(function,point,i,epsilon) for i in range(len(point))]
 
-class OptimizationMethods:
-
     def half_segment(self,function,a,b,delta=None,epsilon=None,max_iter=None):
         if(delta==None): delta=1e-9
         if(epsilon==None): epsilon=1e-8
@@ -40,13 +43,13 @@ class OptimizationMethods:
             x_2=(a+b+delta)/2
             if(function(x_1)<=function(x_2)): b=x_2
             else: a=x_1
-        return min(function(x_1),function(x_2))
+        return (x_1,function(x_1)) if function(x_1)<=function(x_2) else (x_2,function(x_2))
 
-    def golden_ration_naive(self,function,a,b,epsilon=None,max_iter=None):
-        if(epsilon==None): epsilon=1e-6
+    def golden_ratio_naive(self,function,a,b,epsilon=None,max_iter=None):
+        if(epsilon==None): epsilon=1e-8
         golden_1 = (3 - math.sqrt(5)) / 2
         golden_2 = (math.sqrt(5) - 1) / 2
-        if(max_iter==None): max_iter=(int)(math.log(epsilon/(b-a))/math.log(golden_2))+1
+        if(max_iter==None): max_iter=2*(int)(math.log(epsilon/(b-a))/math.log(golden_2))+1
         for i in range(max_iter):
             x_1=a+(b-a)*golden_1
             x_2=a+(b-a)*golden_2
@@ -56,8 +59,20 @@ class OptimizationMethods:
             else: 
                 a=x_1
                 x=x_2
-        return min(function(a+b-x),function(x))
-
-    def gradient_iter(self,function,a,b,delta=None,epsilon=None,max_iter=None):
-        
-        return 0
+        return (a+b-x,function(a+b-x)) if function(a+b-x)<=function(x) else (x,function(x))
+ 
+    def gradient_method(self,function,a,b,epsilon=None):
+        if(epsilon==None): epsilon=1e-8
+        x0=[(i+j)/2 for i,j in zip(a,b)]
+        while(self.gradient_zero(self.gradient(function,x0),epsilon)==False):
+            #print(self.gradient(function,x0),'grad')
+            #print(self.gradient_zero(self.gradient(function,x0),epsilon))
+            #print(x0,'x0')
+            def g_k(a_k):
+                #print(x0)
+                return function([x-a_k*y for (x,y) in zip(x0,self.gradient(function,x0))])
+            #print(g_k(1))
+            alpha=self.golden_ratio_naive(g_k,0,100)
+            #print(alpha)
+            x0=[x-alpha[0]*y for (x,y) in zip(x0,self.gradient(function,x0))]
+        return (x0,function(x0))
