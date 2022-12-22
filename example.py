9f7dcdd773970fba264b74a32fc3c51a3b2b5c32
Артём Костин <superriki228228@gmail.com>
2022-12-22 19:24:08 +0300

    implement projection gradient method

--- example.py
+++ example.py
@@ -2,11 +2,11 @@ from mlib import OptimizationMethods
 import numpy as np
 import math
 
-# def f(x):
-#     return x[0]*x[1]+3*x[0]*x[2]-2*x[1]+np.sin(x[2])
+def k(x):
+    return x[0]**2+np.sin(x[1])
 
 def f(x):
-    return x[0]**2+np.sin(x[1])
+    return x[0]**2+x[1]**2
 
 #print(OptimizationMethods.half_segment(function=f,a=0,b=1))
 
@@ -37,6 +37,63 @@ def g(x):
 
 #print(o.half_segment(g,1,2))
 #print(o.golden_ration_naive(g,1,2))
-print(o.gradient_method(f,[-1,-1],[2,2]))
+
+def k(x):
+    return x[0]**2+np.sin(x[1])
+
+def f(x):
+    return x[0]**2+x[1]**2
+
+def m(x):
+    return x[0]+x[1]
+
+print(o.gradient_method(k,[-1,-1],[2,2]))
+print(o.projection_gradient_method(k,[-1,-1],[2,2]))
+
 
 #print([0]*3)
+#x=[1,2]
+#print(eval('2*np.sin(x[0])+np.sinh(x[1])**2'))
+
+#input=input()
+#print(input)
+
+# def a(x):
+#     return eval(input)
+
+#print(o.gradient_method(a,[-1,-1],[2,2]))
+# x=[1,2]
+# a=[1,3,13]
+
+# print(a[:len(x)])
+# print(a[len(x):])
+
+# a=[1,2]
+
+# b=[3,5]
+
+# print(b+[0])
+
+# print(b)
+
+a=[-1,-1]
+b=[2,2]
+
+x0=[(i+j)/2 for i,j in zip(a,b)]
+y0=np.sqrt(sum([(i-j)**2 for (i,j) in zip(a,b)]))/2
+# print(x0)
+# print(y0)
+
+# def g_k(xa):
+#     return sum([(x1-x_k+xa[len(x0):][0]*y)**2 for (x1,x_k,y) in zip(xa[:len(x0)],x0,o.gradient(f,x0))])
+
+# print(g_k([0,0,1]))
+# xa1=[0,0,1]
+# print(xa1)
+# print(x0)
+# print(o.gradient(f,x0))
+# print(xa1[:len(x0)])
+# print(xa1[len(x0):][0])
+
+#print(np.pi**2/4+np.pi/2+1/2)
+#print(9/2)
--- mlib.py
+++ mlib.py
@@ -1,16 +1,28 @@
 import numpy as np
 import math
+import time
 
 class OptimizationMethods:
 
-    def gradient_zero(self,grad,epsilon):
-        for i in range(len(grad)):
-            if(grad[i]>=epsilon):
+    def list_zero(self,a,accuracy):
+        for i in range(len(a)):
+            if(a[i]>=accuracy):
                 return False
         return True
 
     def add_value(self,list,index,value):
         return list[:index]+[list[index]+value]+list[index+1:]
+
+    def distance(self,a,b):
+        return np.sqrt(sum([(x-y)**2 for (x,y) in zip(a,b)]))
+
+    def projection_ball(self,u,u0,R):
+        if(self.distance(u,u0)<=R):
+            w=u
+        else:
+            w=[x+(y-x)*R/self.distance(u,u0) for (x,y) in zip(u0,u)]
+        return w
+
 
     def first_derivative_1_var(self,function,point,epsilon=None):
         if(epsilon==None): epsilon=1e-8
@@ -61,18 +73,67 @@ class OptimizationMethods:
                 x=x_2
         return (a+b-x,function(a+b-x)) if function(a+b-x)<=function(x) else (x,function(x))
  
-    def gradient_method(self,function,a,b,epsilon=None):
+    def gradient_method(self,function,a,b,epsilon=None,delta=None):
         if(epsilon==None): epsilon=1e-8
+        if(delta==None): delta=1e-8
         x0=[(i+j)/2 for i,j in zip(a,b)]
-        while(self.gradient_zero(self.gradient(function,x0),epsilon)==False):
-            #print(self.gradient(function,x0),'grad')
-            #print(self.gradient_zero(self.gradient(function,x0),epsilon))
-            #print(x0,'x0')
-            def g_k(a_k):
-                #print(x0)
-                return function([x-a_k*y for (x,y) in zip(x0,self.gradient(function,x0))])
-            #print(g_k(1))
-            alpha=self.golden_ratio_naive(g_k,0,100)
-            #print(alpha)
-            x0=[x-alpha[0]*y for (x,y) in zip(x0,self.gradient(function,x0))]
+        delta1=[1]*len(x0)
+        iter=0
+        while(self.list_zero(self.gradient(function,x0),epsilon)==False and self.list_zero(delta1,delta)==False):
+            #print(iter)
+            iter=iter+1
+            def func1(a):
+                return function([x-a*y for (x,y) in zip(x0,self.gradient(function,x0))])
+            alpha=self.golden_ratio_naive(func1,0,1000)[0]
+            result=[x-alpha*y for (x,y) in zip(x0,self.gradient(function,x0))]
+            delta1=[abs(x-y) for (x,y) in zip(x0,result)]
+            x0=result
         return (x0,function(x0))
+    
+    def projection_gradient_method(self,function,a,b,epsilon=None,delta=None):
+        if(epsilon==None): epsilon=1e-8
+        if(delta==None): delta=1e-8
+        x0=[(x+y)/2 for x,y in zip(a,b)]
+        rad=self.distance(a,b)/2
+        u0=x0
+        delta1=[1]*len(x0)
+        iter=0
+        while(self.list_zero(self.gradient(function,x0),epsilon)==False and self.list_zero(delta1,delta)==False):
+            #print(iter)
+            iter=iter+1
+            #print(self.gradient(function,x0),'grad')
+            #print(self.list_zero(self.gradient(function,x0),epsilon))
+            # def func1(a_k):
+            #     #print(x0)
+            #     return function([x-a_k*y for (x,y) in zip(x0,self.gradient(function,x0))])
+            def func1(a):
+                u=[x-a*y for (x,y) in zip(x0,self.gradient(function,x0))]
+                return function(self.projection_ball(u,u0,rad))
+            #print(g_k(1))
+            alpha=self.golden_ratio_naive(func1,0,1000)[0]
+            #alpha=1/(iter+1)
+            #print(alpha,'alpha')
+            #print(x0,'x0')
+            # def func2(x):
+            #     #print(x0)
+            #     #return abs(xa[:len(x0)]-)
+            #     return sum([(x1-x_k+alpha*y)**2 for (x1,x_k,y) in zip(x,x0,self.gradient(function,x0))])
+            #print(g_k(1))
+            #alpha=self.golden_ratio_naive(g_k,0,1000)[0]
+            #alpha=self.half_segment(g_k,0,100)
+            #print(alpha)
+            #result=self.gradient_method(func2,a,b)
+            u=[x-alpha*y for (x,y) in zip(x0,self.gradient(function,x0))]
+            result=self.projection_ball(u,u0,rad)
+            #print(result)
+            #print(x0)
+            #print(result,'x')
+            delta1=[abs(x-y) for (x,y) in zip(x0,result)]
+            #print(delta1,'delta1')
+            #print(self.list_zero(delta1,delta))
+            #print(self.gradient(function,x0),'grad')
+            #print(self.list_zero(self.gradient(function,x0),epsilon))
+            #print(delta1,'delta')
+            x0=result
+        return (x0,function(x0))
+
